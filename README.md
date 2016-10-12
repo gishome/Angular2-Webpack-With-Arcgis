e60a430235d84882c1b58b1698447a99cbbb9d47
dingyh <dyh333@gmail.com>
2016-10-12 10:01:51 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,72 @@
+# Angular2 Webpack With Arcgis
+   this repo is a demo to show how to load arcgis js api based on Angular2 Webpack Starter
+
+# Angular2 Webpack Starter (https://github.com/AngularClass/angular2-webpack-starter)
+
+
+
+# License
+ [MIT](/LICENSE)
+
+# steps of loading arcgis js api
+
+1. npm install @types/arcgis-js-api
+2. npm install @types/dojo
+
+
+3. Remove the following script from index.html
+  <% if (webpackConfig.metadata.isDevServer && webpackConfig.metadata.HMR !== true) { %>
+  <!-- Webpack Dev Server reload -->
+  <script src="/webpack-dev-server.js"></script>
+  <% } %>
+
+4. Add the following in the index.html
+
+  <script>
+    window.dojoConfig = {
+      async: true,
+      isDebug: true
+    };
+  </script>
+  <script src="https://js.arcgis.com/3.18/"></script>
+  <script>
+    setTimeout(function(){ 
+      require([
+        "polyfills.bundle.js", 
+        "vendor.bundle.js",
+        "main.bundle.js"
+      ], function (polyfills, vendor, main) {}); 
+    }, 4000);
+    
+  </script>  
+
+  5. Update webpack.common.js
+
+      new HtmlWebpackPlugin({
+        template: 'src/index.html',
+        chunks: [''],
+        excludeChunks: ['polyfills', 'vendor', 'main']
+      }),
+
+
+  6. Add in webpack.common.js
+
+      externals: [
+       
+        function(context, request, callback) {
+            if (/^dojo/.test(request) ||
+                /^dojox/.test(request) ||
+                /^dijit/.test(request) ||
+                /^esri/.test(request)
+            ) {
+                return callback(null, "amd " + request);
+            }
+            callback();
+        }
+    ],
+    devtool: 'source-map'
+
+7. In webpack.dev.js
+
+      // library: 'ac_[name]',
+      libraryTarget: 'umd',    
---
+++ src/app/about/about.component.ts
@@ -0,0 +1,80 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import Map = require('esri/map');
+import esriBasemaps = require('esri/basemaps');
+/*
+ * We're loading this component asynchronously
+ * We are using some magic with es6-promise-loader that will wrap the module with a Promise
+ * see https://github.com/gdi2290/es6-promise-loader for more info
+ */
+
+console.log('`About` component loaded asynchronously');
+
+@Component({
+  selector: 'about',
+  styles: [`
+  `],
+  template: `
+   <h1>About</h1>
+    <div id='content' [ngStyle]="{'height': '300px', 'background-color': 'blue'}"></div>
+    <div>
+      For hot module reloading run
+      <pre>npm run start:hmr</pre>
+    </div>
+    <div>
+      <h3>
+        patrick@AngularClass.com
+      </h3>
+    </div>
+    <pre>this.localState = {{ localState | json }}</pre>
+  `
+})
+export class About {
+  localState: any;
+  constructor(public route: ActivatedRoute) {
+
+  }
+
+  ngOnInit() {
+    this.route
+      .data
+      .subscribe((data: any) => {
+        // your resolved data from route
+        this.localState = data.yourData;
+      });
+
+    console.log('hello `About` component');
+    // static data that is bundled
+    // var mockData = require('assets/mock-data/mock-data.json');
+    // console.log('mockData', mockData);
+    // if you're working with mock data you can also use http.get('assets/mock-data/mock-data.json')
+    this.asyncDataWithWebpack();
+  }
+
+
+  ngAfterViewInit() {
+    let map = new Map("content", {
+        basemap: "topo",
+        center: [-105.255, 40.022],
+        zoom: 13
+      });
+  }
+
+
+  asyncDataWithWebpack() {
+    // you can also async load mock data with 'es6-promise-loader'
+    // you would do this if you don't want the mock-data bundled
+    // remember that 'es6-promise-loader' is a promise
+    setTimeout(() => {
+
+      System.import('../../assets/mock-data/mock-data.json')
+        .then(json => {
+          console.log('async mockData', json);
+          this.localState = json;
+        });
+
+    });
+  }
+
+}
